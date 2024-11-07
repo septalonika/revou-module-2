3d38ba1cc268a9efdb670ac26ed3365892e5633d
septalonika <andikabeatbox@gmail.com>
2024-11-08 01:45:01 +0700

    add content include to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
-tailwind.config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./assets/css/style.css", "./src/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
         mint: "#3EB489",
+        primary: "#44576d",
       },
     },
   },
+  plugins: [],
 };
